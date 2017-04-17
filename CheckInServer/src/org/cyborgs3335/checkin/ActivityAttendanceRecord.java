d9c7e4b1b746ae35c0991276ed22ed152cec2a8f
Brian Macy <macybk@gmail.com>
2017-04-16 21:16:18 -0500

    Deprecate ActivityAttendanceRecord class, since it's not used.

--- CheckInServer/src/org/cyborgs3335/checkin/ActivityAttendanceRecord.java
+++ CheckInServer/src/org/cyborgs3335/checkin/ActivityAttendanceRecord.java
@@ -1,20 +1,29 @@
 package org.cyborgs3335.checkin;
 
+/**
+ * @deprecated This is probably not the best approach.  Will leave for the time being, but is not currently used.
+ * @author brian
+ *
+ */
+@Deprecated
 public class ActivityAttendanceRecord extends AttendanceRecord {
 
   private static final long serialVersionUID = -252846051460270585L;
 
   private final CheckInActivity activity;
 
+  @Deprecated
   public ActivityAttendanceRecord(Person person) {
     this(person, null);
   }
 
+  @Deprecated
   public ActivityAttendanceRecord(Person person, CheckInActivity activity) {
     super(person);
     this.activity = activity;
   }
 
+  @Deprecated
   public CheckInActivity getActivity() {
     return activity;
   }
